9d0764d135b150d7bf0a71cee8854a4813ca58bf
Archiee07 <155173854+Archiee07@users.noreply.github.com>
2024-01-05 12:58:04 +0100

    Last update
    Last update

--- src/actions/actionTypes.jsx
+++
@@ -1,5 +0,0 @@
-export const setIsUserLoggedIn = 'setIsUserLoggedIn';
-export const setUserEmailId = 'setUserEmailId';
-export const setUserId = 'setUserId';
-export const setUserName = 'setUserName';
-export const setUserRole = 'setUserRole';
